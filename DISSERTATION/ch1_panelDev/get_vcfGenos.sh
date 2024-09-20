6bfab0ad3fd13566ed423e06a9e0c4c030ec3d1c
Rachel Voyt <rachelvoyt@pop-os.localdomain>
2024-09-20 10:35:48 -0500

    shell script to extract genos from vcfs on server

---
+++ DISSERTATION/ch1_panelDev/get_vcfGenos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# definitions
+base_url="https://export.uppmax.uu.se/snic2022-6-144/share/tmp_dir_vcfs/"
+loci_list="/home/rachelvoyt/Documents/UT-Grad/Development/repos/tamGenetics_primatesPeru/DISSERTATION/ch1_panelDev/lociList_bcftools.txt"
+output_dir="/home/rachelvoyt/Documents/UT-Grad/Development/repos/tamGenetics_primatesPeru/DISSERTATION/ch1_panelDev/00_dataRaw"
+
+# loop function
+for i in {1..23}
+do 
+    # define url for individual vcf file
+    vcf_url="$base_url/interval_${i}.vcf.gz"
+
+    # define output for individual vcf file
+    output_file="$output_dir/interval_${i}_genos.txt"
+
+    # extract genos for loci in list, output to txt file
+    bcftools query -H -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT\t%DP\t%GQ]\n' -R "$loci_list" "$vcf_url" > "$output_file"
+
+    # print message indicating file has been processed
+    echo "Processed interval_${i}.vcf.gz -> $output_file"
+done
