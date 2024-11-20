0d36a1ca838f6f2fc41b54805d6ff6c4df23d207
Rachel Voyt <rachelvoyt@pop-os.localdomain>
2024-11-20 02:16:53 -0600

    add my version of this theme

--- project_scripts/ggplot_theme_Publication-2.R
+++ project_scripts/ggplot_theme_Publication-2.R
@@ -1,5 +1,38 @@
+# RV version: using theme_bw as base
+theme_Publication_rv <- function(base_size=14, base_family="sans") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.2), hjust = 0.5, margin = margin(0,0,20,0)),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            #panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(), 
+            axis.line.x = element_line(colour="black"),
+            axis.line.y = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="gray92"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            #legend.direction = "horizontal",
+            legend.box = "vertical",
+            legend.key.size= unit(0.5, "cm"),
+            #legend.margin = unit(0, "cm"),
+            legend.title = element_text(face="italic"),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            #strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
 
-
+# all below are the original scripts--
 theme_Publication <- function(base_size=14, base_family="sans") {
       library(grid)
       library(ggthemes)
@@ -21,8 +54,8 @@ theme_Publication <- function(base_size=14, base_family="sans") {
                panel.grid.minor = element_blank(),
                legend.key = element_rect(colour = NA),
                legend.position = "bottom",
-               legend.direction = "horizontal",
-               legend.box = "vetical",
+               #legend.direction = "horizontal",
+               legend.box = "vertical",
                legend.key.size= unit(0.5, "cm"),
                #legend.margin = unit(0, "cm"),
                legend.title = element_text(face="italic"),
